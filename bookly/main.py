fa49eaa5d4cd212618ee2748ba527b86f84385f9
PUSHPARAJ <135199928+pushparajlitmus7@users.noreply.github.com>
2025-07-17 12:05:42 +0530

    Initial commit

---
+++ bookly/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI,Header
+from typing import Optional
+from pydantic import BaseModel
+
+app=FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message":"Hello, World!"}
+
+
+@app.get('/greet')
+async def greet(name: Optional[str]="User",age:int=0) -> dict:
+    return {"message": f"Hello, {name}!","age": age}
+
+
+class BookCreateModel(BaseModel):
+    title:str
+    author:str
+    
+@app.post('/create_book')
+async def create_book(book_data:BookCreateModel):
+    return {"title": book_data.title, "author": book_data.author}
+
+
+@app.get('/headers')
+async def get_headers(
+    accept:str=Header(None),
+    content_type = Header(None)
+):
+    request_headers = {}
+    request_headers['Accept'] = accept
+    request_headers['Content-Type'] = content_type  
+    return request_headers
